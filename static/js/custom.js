0a698a6fea05911580d6f4ba1c9d6045b452d310
Niklas Buse <nicki_bu@gmx.de>
2021-05-11 20:07:07 +0200

    Add: Navbar Styling, Scrolling

--- static/js/custom.js
+++ static/js/custom.js
@@ -7,3 +7,14 @@ function changeVisibility(item){
         x.className = "w3-show"
     }
 }
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        document.getElementById("navbar").style = "background-color:rgba(15, 15, 15, 0.98); transition-property: background-color; transition-duration: 1s;"
+    } 
+    else {
+        document.getElementById("navbar").style = "background-color:rgba(15, 15, 15, 0.85); transition-property: background-color; transition-duration: 1s;"
+  }
+}
